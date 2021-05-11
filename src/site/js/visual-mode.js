220dd2b837b17d54e70bf5d4f252e4ed2dfe4bd7
Joshua Zeltman <joshua.zeltman@gmail.com>
2021-05-11 18:10:26 -0300

    Update visual-mode.js
    Removing dark/light mode for now until colors are fixed

--- src/site/js/visual-mode.js
+++ src/site/js/visual-mode.js
@@ -2,6 +2,9 @@
 let matched = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
 document.addEventListener('DOMContentLoaded', () => {
+  /* Eliminating for now
   if (matched) document.documentElement.classList.add('dark');
   else document.documentElement.classList.add('light');
+  */
+  document.documentElement.classList.add('light');
 })
