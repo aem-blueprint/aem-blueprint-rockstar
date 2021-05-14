6e30e47bc982d7d76ee8bb237717aea854a05fab
Joshua Zeltman <joshua.zeltman@gmail.com>
2021-05-14 16:43:36 -0300

    empty commit to trigger build

--- src/site/js/visual-mode.js
+++ src/site/js/visual-mode.js
@@ -4,7 +4,6 @@ let matched = window.matchMedia('(prefers-color-scheme: dark)').matches;
 document.addEventListener('DOMContentLoaded', () => {
   /* Eliminating for now
   if (matched) document.documentElement.classList.add('dark');
-  else document.documentElement.classList.add('light');
-  */
+  else document.documentElement.classList.add('light');*/
   document.documentElement.classList.add('light');
 })
